a6e06a60f75f01539859242f667f311a036d112c
Rupam0106 <rupam.gupta@rudrainnovative.in>
2023-08-30 19:01:43 +0530

    create a page of frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import ExpenseFilter from "./components/ExpenseFilter";
+import ExpenseForm from "./components/ExpenseForm";
+
+function App() {
+  return (
+    <>
+      <ExpenseForm />
+      <ExpenseFilter />
+    </>
+  );
+}
+
+export default App;
